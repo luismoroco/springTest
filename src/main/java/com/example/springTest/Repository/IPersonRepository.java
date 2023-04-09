1eb81af7cd45e8dc52ad835f9764d00ddef4e5c2
lmoroco <lmorocoramos@gmail.com>
2023-04-08 19:11:02 -0500

    init repository and init MVC architecture

--- src/main/java/com/example/springTest/Repository/IPersonRepository.java
+++ src/main/java/com/example/springTest/Repository/IPersonRepository.java
@@ -4,5 +4,5 @@ import com.example.springTest.Entity.Persona;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IPersonRepository extends CrudRepository<Persona, Long> {
-
+    /**/
 }
